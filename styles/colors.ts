e83f5f8c99032004630e32a51bf51023957aac91
sookyeongyeom <sookyeong.dev@gmail.com>
2023-01-05 17:55:26 +0900

    Feature/styles-setting
    * Feat: style constants
    
    * Feat: svg constants
    
    * Feat: font-family as pretendard

---
+++ styles/colors.ts
@@ -0,0 +1,32 @@
+export const Colors = {
+	white: '#FFFFFF',
+
+	red100: '#F6DCDB',
+	red500: '#DC746D',
+	red700: '#CC5750',
+	red900: '#B7443C',
+
+	gray100: '#EDEEF3',
+	gray200: '#E4E7EE',
+	gray300: '#D6DAE4',
+	gray400: '#C0C4CF',
+	gray500: '#ADB3C1',
+	gray600: '#9DA4B5',
+	gray700: '#8D95A8',
+	gray800: '#6B748B',
+	gray900: '#383C46',
+	gray950: '#2E3139',
+	gray990: '#1C1E23',
+
+	blue50: '#EAF1FF',
+	blue100: '#DAE6FF',
+	blue300: '#ABC8FF',
+	blue500: '#7DA9FF',
+	blue700: '#6894EB',
+	blue900: '#3F74DB',
+
+	blue500twenty: 'rgba(125, 169, 255, 0.2)',
+
+	background: '#F4F6F8,',
+	shade: 'rgba(20, 20, 22, 0.53),',
+} as const;
--- styles/global.ts
+++ styles/global.ts
@@ -9,7 +9,9 @@ export const GlobalStyle = createGlobalStyle`
   }
 
   html, body {
-    font-family: 'Arial';
+    font-family: Pretendard, 'Pretendard Variable', -apple-system, BlinkMacSystemFont, system-ui,
+		Roboto, 'Helvetica Neue', 'Segoe UI', 'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic',
+		'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', sans-serif;
   }
 
   * {
