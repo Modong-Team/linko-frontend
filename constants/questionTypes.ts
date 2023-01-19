aa7ca08fe84a9b42b5c8dc7d826c0f07de3d4ff5
sookyeongyeom <sookyeong.dev@gmail.com>
2023-01-19 23:29:56 +0900

    Feat: question labels as constants

--- constants/questionTypes.ts
+++ constants/questionTypes.ts
@@ -3,3 +3,9 @@ export const QuestionTypes = {
 	singleSelectQuestion: 2,
 	multiSelectQuestion: 3,
 } as const;
+
+export const QuestionLabels = {
+	1: '주관식',
+	2: '단일 선택',
+	3: '복수 선택',
+} as const;
