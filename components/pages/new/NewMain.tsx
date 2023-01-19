d5d1fb9bc2307cf4e7ec0ec8e0d769a7d4a063ee
sookyeongyeom <sookyeong.dev@gmail.com>
2023-01-19 14:16:53 +0900

    Feat: new main

--- components/pages/new/NewMain.tsx
+++ components/pages/new/NewMain.tsx
@@ -1,9 +1,15 @@
 import styled from 'styled-components';
+import { Colors } from '../../../styles/colors';
 
 export default function NewMain() {
 	return <S.Container>NewMain</S.Container>;
 }
 
 namespace S {
-	export const Container = styled.div``;
+	export const Container = styled.div`
+		background-color: ${Colors.white};
+		border-radius: 0.8rem;
+		border: 0.1rem solid ${Colors.gray200};
+		padding: 4rem;
+	`;
 }
