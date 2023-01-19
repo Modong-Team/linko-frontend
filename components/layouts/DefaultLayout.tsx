5128a41e9ceabf03af7e0e31f34516a134411b72
sookyeongyeom <sookyeong.dev@gmail.com>
2023-01-19 13:44:30 +0900

    Feat: new indicator

--- components/layouts/DefaultLayout.tsx
+++ components/layouts/DefaultLayout.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { Colors } from '../../styles/colors';
 
 export default function DefaultLayout({ children }: ChildrenType) {
 	return <S.Layout>{children}</S.Layout>;
@@ -8,6 +9,6 @@ namespace S {
 	export const Layout = styled.div`
 		width: 100vw;
 		min-height: 100vh;
-		background-color: aliceblue;
+		background-color: ${Colors.background};
 	`;
 }
