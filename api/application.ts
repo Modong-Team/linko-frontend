9891f63effb734e8e4baca46f2126b95740e237b
sookyeongyeom <sookyeong.dev@gmail.com>
2023-01-20 19:12:47 +0900

    Feat: types & api for application get

--- api/application.ts
+++ api/application.ts
@@ -2,6 +2,11 @@ import request from './core/index';
 
 const baseUrl = '/application';
 
+export const getApplication = (applicationId: number) => {
+	const url = `${baseUrl}/${applicationId}`;
+	return request.get<ResponseApplication.Get>(url);
+};
+
 export const postApplication = (data: RequestApplication.Post) => {
 	const url = `${baseUrl}`;
 	return request.post<ResponseApplication.Post, RequestApplication.Post>(url, data);
