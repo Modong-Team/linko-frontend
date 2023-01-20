bc67f67ad40f4dc0c233531ef4641747d2824c44
sookyeongyeom <sookyeong.dev@gmail.com>
2023-01-20 18:56:22 +0900

    Feat: new complete publishing

---
+++ components/pages/new/NewComplete.tsx
@@ -0,0 +1,57 @@
+import { SC } from '../../../styles/styled';
+import { Icons } from '../../../styles/icons';
+import styled from 'styled-components';
+import { Fonts } from '../../../styles/fonts';
+import { Colors } from '../../../styles/colors';
+import { svgCopy } from '../../../styles/svgs';
+import useRouteToPath from '../../../hooks/useRouteToPath';
+import { Paths } from '../../../constants/paths';
+
+export default function NewComplete() {
+	const onRouteToMain = useRouteToPath(Paths.main);
+	return (
+		<S.Container>
+			<h1>
+				{Icons.hands}
+				<br />
+				공고 생성이 완료되었습니다!
+			</h1>
+			<div>www.modong.com/3H5os4{svgCopy}</div>
+			<div>
+				<button>수정하기</button>
+				<button onClick={onRouteToMain}>홈으로 돌아가기</button>
+			</div>
+		</S.Container>
+	);
+}
+
+namespace S {
+	export const Container = styled(SC.NewMainContainer)`
+		display: flex;
+		flex-direction: column;
+		gap: 2.4rem;
+		align-items: center;
+
+		> h1 {
+			${Fonts.heading24bold}
+			text-align: center;
+		}
+
+		> div:first-of-type {
+			${Fonts.subtitle16medium}
+			color: ${Colors.gray700};
+			background-color: ${Colors.gray100};
+			padding: 0.3rem 2.4rem;
+			border-radius: 0.4rem;
+			display: flex;
+			align-items: center;
+			gap: 0.9rem;
+			cursor: pointer;
+		}
+
+		> div:last-of-type {
+			display: flex;
+			gap: 1.2rem;
+		}
+	`;
+}
--- components/pages/new/NewCompletePage.tsx
+++ components/pages/new/NewCompletePage.tsx
@@ -1,3 +1,24 @@
+import styled from 'styled-components';
+import NewComplete from './NewComplete';
+import NewIndicator from './NewIndicator';
+
 export default function NewCompletePage() {
-	return <>New Complete Page</>;
+	return (
+		<S.Container>
+			<NewIndicator isComplete />
+			<NewComplete />
+		</S.Container>
+	);
+}
+
+namespace S {
+	export const Container = styled.div`
+		display: grid;
+		grid-template-columns: 19.4rem 71.2rem 19.4rem;
+		row-gap: 1.6rem;
+		width: fit-content;
+		margin: 0 auto;
+		justify-content: center;
+		padding-top: 4rem;
+	`;
 }
