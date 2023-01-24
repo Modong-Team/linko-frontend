e05f68457bec4e396adcf93bd6ffdec9ec8ae07e
sookyeongyeom <sookyeong.dev@gmail.com>
2023-01-25 01:27:50 +0900

    Feat: post applicant on submit reply

--- api/applicant.ts
+++ api/applicant.ts
@@ -2,7 +2,7 @@ import request from './core/index';
 
 const baseUrl = '/applicant';
 
-export const postApplication = (data: RequestApplicant.Post) => {
+export const postApplicant = (data: RequestApplicant.Post) => {
 	const url = `${baseUrl}`;
 	return request.post<ResponseApplicant.Post, RequestApplicant.Post>(url, data);
 };
--- components/pages/reply/ReplyPageButtons.tsx
+++ components/pages/reply/ReplyPageButtons.tsx
@@ -1,18 +1,26 @@
 import styled from 'styled-components';
 import NewPageButton from '../../buttons/NewPageButton';
 import useApplication from '../../../hooks/useApplication';
+import useAnswers from '../../../hooks/useAnswers';
+import { postApplicant } from '../../../api/applicant';
 
 export default function ReplyPageButtons({ page, onPrevPage, onNextPage }: NewPageButtonsProps) {
+	const { answers } = useAnswers();
 	const { application } = useApplication();
 
 	const checkIsLastPage = () => application.data.forms.length - 1 === page;
+
+	const onSubmit = async () => {
+		const post = await postApplicant(answers);
+		console.log(post);
+	};
 
 	return (
 		<S.Container>
 			<NewPageButton label='이전' onClick={onPrevPage} isLeft isHidden={page === -1} />
 			<NewPageButton
 				label={checkIsLastPage() ? '지원서 제출' : '다음'}
-				onClick={checkIsLastPage() ? console.log : onNextPage}
+				onClick={checkIsLastPage() ? onSubmit : onNextPage}
 				isRight
 				isHidden={false}
 			/>
