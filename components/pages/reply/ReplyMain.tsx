bbc7eef90832a14fc853734b4e54bed41deff6e3
sookyeongyeom <sookyeong.dev@gmail.com>
2023-01-24 18:33:20 +0900

    Feat: reply main

---
+++ components/pages/reply/ReplyMain.tsx
@@ -0,0 +1,10 @@
+import { SC } from '../../../styles/styled';
+import ReplyEssentials from './ReplyEssentials';
+
+export default function ReplyMain() {
+	return (
+		<SC.NewMainContainer>
+			<ReplyEssentials />
+		</SC.NewMainContainer>
+	);
+}
--- components/pages/reply/ReplyPage.tsx
+++ components/pages/reply/ReplyPage.tsx
@@ -1,3 +1,34 @@
+import ReplyMeta from './ReplyMeta';
+import ReplyMain from './ReplyMain';
+import NewPageButtons from '../new/NewPageButtons';
+import styled from 'styled-components';
+
 export default function ReplyPage() {
-	return <>Reply</>;
+	return (
+		<S.Container>
+			<ReplyMeta />
+			<ReplyMain />
+			<NewPageButtons
+				page={0}
+				onPrevPage={function (): void {
+					throw new Error('Function not implemented.');
+				}}
+				onNextPage={function (): void {
+					throw new Error('Function not implemented.');
+				}}
+			/>
+		</S.Container>
+	);
+}
+
+namespace S {
+	export const Container = styled.div`
+		width: 71.2rem;
+		margin: 0 auto;
+		padding-top: 6rem;
+		padding-bottom: 12rem;
+		display: flex;
+		flex-direction: column;
+		gap: 1.6rem;
+	`;
 }
