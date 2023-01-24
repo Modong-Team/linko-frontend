10de82aaa34fe0b7a2546d77489a4c147a67df70
sookyeongyeom <sookyeong.dev@gmail.com>
2023-01-25 02:13:27 +0900

    Feat: types & api for applicant get

--- api/applicant.ts
+++ api/applicant.ts
@@ -2,6 +2,11 @@ import request from './core/index';
 
 const baseUrl = '/applicant';
 
+export const getApplicant = (applicantId: number) => {
+	const url = `${baseUrl}/${applicantId}`;
+	return request.get<ResponseApplicant.Get>(url);
+};
+
 export const postApplicant = (data: RequestApplicant.Post) => {
 	const url = `${baseUrl}`;
 	return request.post<ResponseApplicant.Post, RequestApplicant.Post>(url, data);
