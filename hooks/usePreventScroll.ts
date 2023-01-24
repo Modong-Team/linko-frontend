b7a3df17100d3b0a36e901d6b98124a7b9d3abe3
sookyeongyeom <sookyeong.dev@gmail.com>
2023-01-25 01:53:54 +0900

    Feat: add dep to usePreventScroll hook

--- hooks/usePreventScroll.ts
+++ hooks/usePreventScroll.ts
@@ -1,16 +1,18 @@
 import { useEffect } from 'react';
 
-export default function usePreventScroll() {
+export default function usePreventScroll(isPrevent: boolean) {
 	return useEffect(() => {
-		document.body.style.cssText = `
+		if (isPrevent) {
+			document.body.style.cssText = `
       position: fixed; 
       top: -${window.scrollY}px;
       overflow-y: scroll;
       width: 100%;`;
-		return () => {
+		}
+		if (!isPrevent) {
 			const scrollY = document.body.style.top;
 			document.body.style.cssText = '';
 			window.scrollTo(0, parseInt(scrollY || '0', 10) * -1);
-		};
-	}, []);
+		}
+	}, [isPrevent]);
 }
