0aaa443fb340e5a5b1a06af61da17f402f51dc96
sookyeongyeom <sookyeong.dev@gmail.com>
2023-01-29 16:00:11 +0900

    Feat: loading dots

---
+++ components/shared/LoadingDots.tsx
@@ -0,0 +1,31 @@
+import Lottie from 'react-lottie';
+import LoadingLottieBlue from '../../public/assets/loadingBlue.json';
+import LoadingLottieWhite from '../../public/assets/loadingWhite.json';
+import styled from 'styled-components';
+import { LoadingDotsProps } from '../../@types/client';
+
+export default function LoadingDots({ width, isWhite, isHidden }: LoadingDotsProps) {
+	const options = {
+		loop: true,
+		autoplay: true,
+		animationData: isWhite ? LoadingLottieWhite : LoadingLottieBlue,
+	};
+
+	return (
+		<S.Container isHidden={isHidden}>
+			<Lottie options={options} width={width} isClickToPauseDisabled={true} />
+		</S.Container>
+	);
+}
+
+namespace S {
+	export const Container = styled.div<IsHiddenType>`
+		width: 100%;
+		height: 100%;
+		position: absolute;
+		top: 0;
+		left: 0;
+		visibility: ${(props) => props.isHidden && 'hidden'};
+		opacity: ${(props) => (props.isHidden ? 0 : 1)};
+	`;
+}
