7fbc268256bb7a5bb5194cd2c98caf1c3c62e219
sookyeongyeom <sookyeong.dev@gmail.com>
2023-01-29 16:46:42 +0900

    Feat: add post process logic to useGet hook

--- hooks/useGet.tsx
+++ hooks/useGet.tsx
@@ -3,7 +3,9 @@ import { Dispatch, SetStateAction } from 'react';
 export default async function useGet<T>(
 	api: () => Promise<T>,
 	dispatch: Dispatch<SetStateAction<T | undefined>>,
+	postProcess?: () => any,
 ) {
 	const res = await api();
-	dispatch(res);
+	await dispatch(res);
+	if (postProcess) postProcess();
 }
