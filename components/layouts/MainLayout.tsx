8c4f7b6cb199556d6653b3c8ea44cb691ead0dbb
sookyeongyeom <sookyeong.dev@gmail.com>
2023-01-29 18:58:10 +0900

    Feat: useLocalLoading hook

--- components/layouts/MainLayout.tsx
+++ components/layouts/MainLayout.tsx
@@ -1,11 +1,21 @@
-import { WithHeaderLayout } from '.';
 import { Header } from '../shared';
+import styled from 'styled-components';
+import MainSidebar from '../pages/main/MainSidebar';
+import { Colors } from '../../styles/colors';
+import DefaultLayout from './DefaultLayout';
 
 export default function MainLayout({ children }: ChildrenType) {
 	return (
-		<WithHeaderLayout>
+		<DefaultLayout>
 			<Header isMain />
-			{children}
-		</WithHeaderLayout>
+			<S.Layout>
+				<MainSidebar />
+				{children}
+			</S.Layout>
+		</DefaultLayout>
 	);
+}
+
+namespace S {
+	export const Layout = styled.div``;
 }
