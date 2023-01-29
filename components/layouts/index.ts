447746ddc69b8ddf1925f70fc1bcae22a2b58942
sookyeongyeom <sookyeong.dev@gmail.com>
2023-01-29 18:56:58 +0900

    Chore: remove useless layout

--- components/layouts/index.ts
+++ components/layouts/index.ts
@@ -3,6 +3,5 @@ import MainLayout from './MainLayout';
 import NewLayout from './NewLayout';
 import ReplyLayout from './ReplyLayout';
 import ViewLayout from './ViewLayout';
-import WithHeaderLayout from './WithHeaderLayout';
 
-export { DefaultLayout, MainLayout, NewLayout, ReplyLayout, ViewLayout, WithHeaderLayout };
+export { DefaultLayout, MainLayout, NewLayout, ReplyLayout, ViewLayout };
