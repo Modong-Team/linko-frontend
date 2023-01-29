e207627c549e6fa8c8c9864511cd8989fbf36fe2
sookyeongyeom <sookyeong.dev@gmail.com>
2023-01-29 23:40:37 +0900

    Feat: main board

---
+++ components/pages/main/MainBoard.tsx
@@ -0,0 +1,50 @@
+import FilterButton from '../../buttons/FilterButton';
+import MainBoardColumn from './MainBoardColumn';
+import styled from 'styled-components';
+
+export default function MainBoard() {
+	return (
+		<S.Container>
+			<S.FilterButtons>
+				<FilterButton
+					label={'모든 지원자'}
+					onClick={() => alert('미구현 피쳐')}
+					onBlur={() => alert('미구현 피쳐')}
+				/>
+				<FilterButton
+					label={'가나다 순'}
+					onClick={() => alert('미구현 피쳐')}
+					onBlur={() => alert('미구현 피쳐')}
+				/>
+			</S.FilterButtons>
+			<S.BoardContainer>
+				<MainBoardColumn />
+				<MainBoardColumn />
+				<MainBoardColumn />
+				<MainBoardColumn />
+			</S.BoardContainer>
+		</S.Container>
+	);
+}
+
+namespace S {
+	export const Container = styled.div`
+		flex-grow: 1;
+		display: flex;
+		flex-direction: column;
+		padding-bottom: 3.2rem;
+	`;
+
+	export const FilterButtons = styled.div`
+		padding: 1.6rem 0;
+		display: flex;
+		gap: 0.6rem;
+	`;
+
+	export const BoardContainer = styled.div`
+		flex-grow: 1;
+		display: grid;
+		grid-template-columns: repeat(4, 1fr);
+		column-gap: 1.2rem;
+	`;
+}
