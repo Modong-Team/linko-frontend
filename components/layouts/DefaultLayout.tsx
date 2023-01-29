4ebdbdc91b329d6479bea37dd240d03e43d18c77
sookyeongyeom <sookyeong.dev@gmail.com>
2023-01-29 16:01:23 +0900

    Feat: apply loading dots to default layout

--- components/layouts/DefaultLayout.tsx
+++ components/layouts/DefaultLayout.tsx
@@ -1,8 +1,15 @@
 import styled from 'styled-components';
 import { Colors } from '../../styles/colors';
+import LoadingDots from '../shared/LoadingDots';
+import { LoadingWidths } from '../../constants/loadingWidths';
 
 export default function DefaultLayout({ children }: ChildrenType) {
-	return <S.Layout>{children}</S.Layout>;
+	return (
+		<S.Layout>
+			{children}
+			<LoadingDots width={LoadingWidths.fullscreen} isHidden={true} />
+		</S.Layout>
+	);
 }
 
 namespace S {
