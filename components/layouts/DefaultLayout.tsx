c44425140481d9a3ace8447ce561dc9ce3499c32
sookyeongyeom <sookyeong.dev@gmail.com>
2023-01-29 16:13:49 +0900

    Feat: connect fullscreen loading dot to global loading status

--- components/layouts/DefaultLayout.tsx
+++ components/layouts/DefaultLayout.tsx
@@ -2,12 +2,14 @@ import styled from 'styled-components';
 import { Colors } from '../../styles/colors';
 import LoadingDots from '../shared/LoadingDots';
 import { LoadingWidths } from '../../constants/loadingWidths';
+import useLoadingStatus from '../../hooks/useLoadingStatus';
 
 export default function DefaultLayout({ children }: ChildrenType) {
+	const { loadingStatus } = useLoadingStatus();
 	return (
 		<S.Layout>
 			{children}
-			<LoadingDots width={LoadingWidths.fullscreen} isHidden={true} />
+			<LoadingDots width={LoadingWidths.fullscreen} isHidden={!loadingStatus.isLoading} />
 		</S.Layout>
 	);
 }
