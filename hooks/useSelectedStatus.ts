0bc4211e44bbe3ca8564f6c0e2399664eab7e362
sookyeongyeom <sookyeong.dev@gmail.com>
2023-01-30 07:33:33 +0900

    Feat: useSelectedStatus & useSelectedApplicants hook

---
+++ hooks/useSelectedStatus.ts
@@ -0,0 +1,17 @@
+import { RootState } from '../modules/index';
+import { useSelector, useDispatch } from 'react-redux';
+import { ApplicantStatusCodeKeys } from '../constants/applicantStatusCode';
+import { selectStatus, requestResetStatus } from '../modules/main/selectedStatus';
+
+export default function useSelectedStatus() {
+	const selectedStatus = useSelector(({ selectedStatus }: RootState) => selectedStatus);
+	const dispatch = useDispatch();
+
+	const onSelectStatus = (
+		applicantStatusCode: typeof ApplicantStatusCodeKeys[keyof typeof ApplicantStatusCodeKeys],
+	) => dispatch(selectStatus(applicantStatusCode));
+
+	const onRequestResetStatus = () => dispatch(requestResetStatus());
+
+	return { selectedStatus, onSelectStatus, onRequestResetStatus };
+}
