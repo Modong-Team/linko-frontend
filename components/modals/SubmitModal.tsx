7dc92f89c878d8f88fa73b8cca16de27ed9ed90b
sookyeongyeom <sookyeong.dev@gmail.com>
2023-02-05 23:34:17 +0900

    Feat: modal styles as styled constant

--- components/modals/SubmitModal.tsx
+++ components/modals/SubmitModal.tsx
@@ -1,12 +1,9 @@
-import styled from 'styled-components';
 import { ButtonSizes, ButtonTypes } from '../../constants/buttons';
-import { Colors } from '../../styles/colors';
-import { Fonts } from '../../styles/fonts';
 import CustomButton from '../buttons/CustomButton';
 import { Icons } from '../../styles/icons';
 import usePreventScroll from '../../hooks/usePreventScroll';
-import { Devices } from '../../styles/devices';
 import useLoadingStatus from '../../hooks/useLoadingStatus';
+import { SC } from '../../styles/styled';
 
 export default function SubmitModal({
 	title,
@@ -18,12 +15,12 @@ export default function SubmitModal({
 	const { loadingStatus } = useLoadingStatus();
 	usePreventScroll(!isHidden);
 	return (
-		<S.ModalBackground isHidden={isHidden}>
-			<S.ModalContainer>
-				<S.Icon>{Icons.pencil}</S.Icon>
-				<S.Title>{title}</S.Title>
-				<S.Subtitle>{description}</S.Subtitle>
-				<S.ButtonWrapper>
+		<SC.ModalBackground isHidden={isHidden}>
+			<SC.ModalContainer>
+				<SC.ModalIcon>{Icons.pencil}</SC.ModalIcon>
+				<SC.ModalTitle>{title}</SC.ModalTitle>
+				<SC.ModalSubtitle>{description}</SC.ModalSubtitle>
+				<SC.ModalButtonWrapper>
 					<CustomButton
 						label='취소'
 						buttonSize={ButtonSizes.large}
@@ -37,70 +34,8 @@ export default function SubmitModal({
 						onClick={onConfirm}
 						isLoading={loadingStatus.isGlobalLoading}
 					/>
-				</S.ButtonWrapper>
-			</S.ModalContainer>
-		</S.ModalBackground>
+				</SC.ModalButtonWrapper>
+			</SC.ModalContainer>
+		</SC.ModalBackground>
 	);
-}
-
-namespace S {
-	export const ModalBackground = styled.div<IsHiddenType>`
-		background-color: ${Colors.shade};
-		width: 100vw;
-		height: 100vh;
-		position: fixed;
-		top: 0;
-		left: 0;
-		z-index: 100;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		transition: 0.3s ease;
-		visibility: ${(props) => props.isHidden && 'hidden'};
-		opacity: ${(props) => (props.isHidden ? 0 : 1)};
-
-		@media ${Devices.mobile} {
-			padding: 2.4rem;
-		}
-	`;
-
-	export const ModalContainer = styled.div`
-		width: 50rem;
-		padding: 2.4rem;
-		padding-top: 4rem;
-		text-align: center;
-		background-color: ${Colors.white};
-		box-shadow: 0px 6px 20px rgba(0, 0, 0, 0.12);
-		border-radius: 1.2rem;
-
-		@media ${Devices.mobile} {
-			padding: 2rem;
-			padding-bottom: 2.4rem;
-		}
-	`;
-
-	export const Icon = styled.div`
-		${Fonts.heading24bold}
-	`;
-
-	export const Title = styled.div`
-		${Fonts.heading24bold}
-		margin-bottom: 0.4rem;
-	`;
-
-	export const Subtitle = styled.div`
-		${Fonts.subtitle16medium}
-		color: ${Colors.gray700};
-		margin-bottom: 2.4rem;
-	`;
-
-	export const ButtonWrapper = styled.div`
-		display: flex;
-		width: 100%;
-		gap: 0.8rem;
-
-		button {
-			width: 100%;
-		}
-	`;
 }
