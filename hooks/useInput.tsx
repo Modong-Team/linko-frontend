8588b39c51c67ea55bfb1cbbd88b876e2a2cca0a
sookyeongyeom <sookyeong.dev@gmail.com>
2023-02-06 01:19:27 +0900

    Fix: useInput return value as array

--- hooks/useInput.tsx
+++ hooks/useInput.tsx
@@ -1,7 +1,9 @@
 import { useState } from 'react';
 
-export default function useInput(initialValue = '') {
+export default function useInput(
+	initialValue = '',
+): [string, (e: React.ChangeEvent<HTMLInputElement>) => void] {
 	const [value, setValue] = useState(initialValue);
 	const onChange = (e: React.ChangeEvent<HTMLInputElement>) => setValue(e.target.value);
-	return { value, onChange };
+	return [value, onChange];
 }
