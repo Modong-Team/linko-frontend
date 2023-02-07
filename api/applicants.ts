d29a00fc28c1f2640a14a24a07563dfc772eec1b
sookyeongyeom <sookyeong.dev@gmail.com>
2023-02-07 19:47:00 +0900

    Feat: api for applicants get

--- api/applicants.ts
+++ api/applicants.ts
@@ -2,7 +2,21 @@ import request from './core/index';
 
 const baseUrl = '/applicants';
 
-export const getApplicants = (applicationId: number) => {
-	const url = `${baseUrl}/${applicationId}`;
+export const getApplicants = (
+	applicationId: number,
+	applicantStatusCode: 1 | 2 | 3 | 4 | 5,
+	page: number,
+	filter: 'all' | 'evaluating' | 'fail',
+	sort: 'name,asc' | 'rate,asc' | 'rate,desc',
+) => {
+	const size = '6';
+	const queries = [
+		'applicantStatusCode=' + applicantStatusCode,
+		'filter=' + filter,
+		'page=' + page,
+		'size=' + size,
+		'sort=' + sort,
+	];
+	const url = `${baseUrl}/${applicationId}?${queries.join('&')}`;
 	return request.get<ResponseApplicants.Get>(url);
 };
