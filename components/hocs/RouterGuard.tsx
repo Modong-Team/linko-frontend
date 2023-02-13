7ed515b63168778fbf7e7fd60212d702e61447ae
sookyeongyeom <sookyeong.dev@gmail.com>
2023-02-13 20:38:11 +0900

    Feat: RouterGuard advanced

--- components/hocs/RouterGuard.tsx
+++ components/hocs/RouterGuard.tsx
@@ -1,23 +1,50 @@
 import { useRouter } from 'next/router';
 import { useState, useEffect } from 'react';
 import { PublicPathsRegex, Paths } from '../../constants/paths';
+import { StorageKeys } from '../../constants/keys';
+import useAuthData from '../../hooks/useAuthData';
+import { postToken } from '../../api/token';
+import useDidMountEffect from '../../hooks/useDidMountEffect';
 
 export default function RouterGuard({ children }: ChildrenType) {
 	const router = useRouter();
-	const [isPermitted, setIsPermitted] = useState(false);
+	const [isRenderPermitted, setIsRenderPermitted] = useState(false);
+	const { authData, onRequestSetAuthData } = useAuthData();
+
+	const onRouteToMain = () => router.push(Paths.main);
+	const onRouteToLogin = () => router.push(Paths.login);
+	const checkIsPublicPath = () => PublicPathsRegex.test(router.pathname);
+	const checkIsAuthed = () => !!authData;
+
+	const onTryRestoreAuthData = async () => {
+		const refreshToken = sessionStorage.getItem(StorageKeys.refreshToken);
+		if (!refreshToken) return onDecideWhetherToRender();
+		try {
+			const post = await postToken({ refreshToken });
+			onRequestSetAuthData(post.data);
+		} catch {
+			sessionStorage.removeItem(StorageKeys.refreshToken);
+			onDecideWhetherToRender();
+		}
+	};
+
+	const onDecideWhetherToRender = () => {
+		if (checkIsPublicPath() !== checkIsAuthed()) setIsRenderPermitted(true);
+		else onRedirectToProperPath();
+	};
+
+	const onRedirectToProperPath = () => {
+		if (checkIsPublicPath() && checkIsAuthed()) onRouteToMain();
+		if (!checkIsPublicPath() && !checkIsAuthed()) onRouteToLogin();
+	};
 
 	useEffect(() => {
-		const isPublicPath = PublicPathsRegex.test(router.pathname);
-		const isAuthTokenExist = sessionStorage.getItem('refreshToken');
+		onTryRestoreAuthData();
+	}, []);
 
-		if (isPublicPath || isAuthTokenExist) {
-			setIsPermitted(true);
-			return;
-		}
+	useDidMountEffect(() => {
+		onDecideWhetherToRender();
+	}, [router.pathname, authData]);
 
-		setIsPermitted(false);
-		router.push(Paths.login);
-	}, [router.pathname]);
-
-	return <>{isPermitted && children}</>;
+	return <>{isRenderPermitted && children}</>;
 }
