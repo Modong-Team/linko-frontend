8a3f9a75a797b1424d917389a197fa37c9ef34f7
sookyeongyeom <sookyeong.dev@gmail.com>
2023-02-15 03:15:31 +0900

    Feat: rename env for front end base url

--- utils/createReplyUrl.ts
+++ utils/createReplyUrl.ts
@@ -1,5 +1,5 @@
 import { Paths } from '../constants/paths';
 
 export default function createReplyUrl(urlId: string) {
-	return process.env.NEXT_PUBLIC_FRONT_END_TEST_SERVER_BASE_URL + `${Paths.reply}/${urlId}`;
+	return process.env.NEXT_PUBLIC_FRONT_END_BASE_URL + `${Paths.reply}/${urlId}`;
 }
