84d68b76d02a1d6abc0c06feef1401687cb8de1c
sookyeong <sookyeong.dev@gmail.com>
2023-02-15 14:44:49 +0900

    Feature/register member validation (#35)
    * Chore: install formik & yup
    
    * Feat: RegisterMemberPage validation logic

--- components/pages/register/RegisterMemberPage.tsx
+++ components/pages/register/RegisterMemberPage.tsx
@@ -2,99 +2,131 @@ import styled from 'styled-components';
 import CustomButton from '../../buttons/CustomButton';
 import ReplyTextInput from '../../inputs/ReplyTextInput';
 import { ButtonTypes, ButtonSizes } from '../../../constants/buttons';
-import useInput from '../../../hooks/useInput';
 import { postRegister } from '../../../api/register';
+import { useFormik } from 'formik';
+import { object, string } from 'yup';
 
 export default function RegisterMemberPage({ clubId }: RegisterMemberPageProps) {
-	const [memberId, onChangeMemberId] = useInput();
-	const [password, onChangePassword] = useInput();
-	const [passwordForCheck, onChangePasswordForCheck] = useInput();
-	const [name, onChangeName] = useInput();
-	const [email, onChangeEmail] = useInput();
-	const [phone, onChangePhone] = useInput();
-
-	const onSubmit = async () => {
-		try {
-			const post = await postRegister({
-				memberId,
-				password,
-				name,
-				email,
-				phone,
-				clubCode: clubId,
-			});
+	const formik = useFormik({
+		initialValues: {
+			memberId: '',
+			password: '',
+			passwordForCheck: '',
+			name: '',
+			email: '',
+			phone: '',
+		},
+		validationSchema: object({
+			memberId: string().min(3).max(20).required(),
+			password: string()
+				.min(6)
+				.max(20)
+				.matches(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{6,}$/)
+				.required(),
+			name: string().required(),
+			email: string().email().required(),
+			phone: string()
+				.length(11)
+				.matches(/^[0-9]+$/)
+				.required(),
+		}),
+		onSubmit: async () => {
+			const post = postRegister({ ...formik.values, clubCode: clubId });
 			console.log(post);
-		} catch {}
+		},
+		validateOnChange: true,
+		validateOnMount: true,
+	});
+
+	const checkIfErrorExist = () => {
+		for (const error in formik.errors) if (error !== '') return true;
+		return false;
 	};
 
 	return (
 		<S.Container>
 			<ReplyTextInput
-				value={memberId}
-				onChange={onChangeMemberId}
+				name={'memberId'}
+				value={formik.values.memberId}
+				isError={formik.touched.memberId && !!formik.errors.memberId}
+				onChange={formik.handleChange}
+				onBlur={formik.handleBlur}
 				label={'아이디 (3~20자)'}
-				errorMessage={'아이디를 제대로 입력하셈'}
-				minLength={3}
+				errorMessage={'아이디를 올바르게 입력해주세요.'}
 				maxLength={20}
-				pattern={'^[a-zA-Z0-9]*$'}
 				isSingleLine
 			/>
 			<ReplyTextInput
-				value={password}
-				onChange={onChangePassword}
+				name={'password'}
+				value={formik.values.password}
+				isError={formik.touched.password && !!formik.errors.password}
+				onChange={formik.handleChange}
+				onBlur={formik.handleBlur}
 				label={'비밀번호 (숫자, 영문, 특수문자 조합 6~20자)'}
-				errorMessage={''}
-				minLength={6}
-				maxLength={20}
+				errorMessage={'비밀번호를 올바르게 입력해주세요.'}
 				type={'password'}
+				maxLength={20}
 				isSingleLine
 			/>
 			<ReplyTextInput
-				value={passwordForCheck}
-				onChange={onChangePasswordForCheck}
+				name={'passwordForCheck'}
+				value={formik.values.passwordForCheck}
+				isError={
+					formik.touched.passwordForCheck &&
+					formik.values.password !== formik.values.passwordForCheck
+				}
+				onChange={formik.handleChange}
+				onBlur={formik.handleBlur}
 				label={'비밀번호 확인'}
-				errorMessage={''}
-				minLength={6}
-				maxLength={20}
+				errorMessage={'비밀번호가 일치하지 않습니다.'}
 				type={'password'}
+				maxLength={20}
 				isSingleLine
 			/>
 			<ReplyTextInput
-				value={name}
-				onChange={onChangeName}
+				name={'name'}
+				value={formik.values.name}
+				isError={formik.touched.name && !!formik.errors.name}
+				onChange={formik.handleChange}
+				onBlur={formik.handleBlur}
 				label={'사용자 이름'}
-				errorMessage={''}
-				isSingleLine
-			/>
-			<ReplyTextInput //
-				value={email}
-				onChange={onChangeEmail}
-				label={'이메일'}
-				errorMessage={''}
-				pattern={'^[a-zA-Z0-9]+@[a-zA-Z0-9]+$'}
+				errorMessage={'이름을 올바르게 입력해주세요.'}
 				isSingleLine
 			/>
 			<ReplyTextInput
-				value={phone}
-				onChange={onChangePhone}
+				name={'email'}
+				value={formik.values.email}
+				isError={formik.touched.email && !!formik.errors.email}
+				onChange={formik.handleChange}
+				onBlur={formik.handleBlur}
+				label={'이메일'}
+				errorMessage={'이메일을 올바르게 입력해주세요.'}
+				isSingleLine
+			/>
+			<ReplyTextInput
+				name={'phone'}
+				value={formik.values.phone}
+				isError={formik.touched.phone && !!formik.errors.phone}
+				onChange={formik.handleChange}
+				onBlur={formik.handleBlur}
 				label={'전화번호 (숫자만 입력)'}
-				errorMessage={''}
-				pattern={'[0-9]+'}
+				errorMessage={'전화번호를 올바르게 입력해주세요.'}
+				maxLength={11}
 				isSingleLine
 			/>
 			<CustomButton
 				label={'가입 완료'}
-				onClick={onSubmit}
+				onClick={formik.handleSubmit}
 				buttonType={ButtonTypes.primary}
 				buttonSize={ButtonSizes.large}
-				disabled={!(memberId && password && passwordForCheck && name && email && phone)}
+				disabled={checkIfErrorExist()}
 			/>
 		</S.Container>
 	);
 }
 
 namespace S {
-	export const Container = styled.div`
+	export const Container = styled.form`
 		> div {
 			margin: 5.5rem 0;
 
