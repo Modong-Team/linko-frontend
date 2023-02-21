3d62e97fe4a32e2c1d912f152da951d21b5ef620
sookyeongyeom <sookyeong.dev@gmail.com>
2023-02-21 20:38:54 +0900

    Feat: add onManuallyChangeValue to useInput

--- hooks/useInput.tsx
+++ hooks/useInput.tsx
@@ -2,9 +2,10 @@ import { useState } from 'react';
 
 export default function useInput(
 	initialValue = '',
-): [string, (e: React.ChangeEvent<HTMLInputElement>) => void, () => void] {
+): [string, (e: React.ChangeEvent<HTMLInputElement>) => void, () => void, (value: string) => void] {
 	const [value, setValue] = useState(initialValue);
 	const onChange = (e: React.ChangeEvent<HTMLInputElement>) => setValue(e.target.value);
 	const onResetValue = () => setValue(initialValue);
-	return [value, onChange, onResetValue];
+	const onManuallyChangeValue = (value: string) => setValue(value);
+	return [value, onChange, onResetValue, onManuallyChangeValue];
 }
