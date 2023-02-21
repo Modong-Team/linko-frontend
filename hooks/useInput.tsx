70b82e15a6e28eec2ba97fbe0f17a12c72f275a0
sookyeongyeom <sookyeong.dev@gmail.com>
2023-02-21 15:03:18 +0900

    Feat: add onResetValue to useInput

--- hooks/useInput.tsx
+++ hooks/useInput.tsx
@@ -2,8 +2,9 @@ import { useState } from 'react';
 
 export default function useInput(
 	initialValue = '',
-): [string, (e: React.ChangeEvent<HTMLInputElement>) => void] {
+): [string, (e: React.ChangeEvent<HTMLInputElement>) => void, () => void] {
 	const [value, setValue] = useState(initialValue);
 	const onChange = (e: React.ChangeEvent<HTMLInputElement>) => setValue(e.target.value);
-	return [value, onChange];
+	const onResetValue = () => setValue(initialValue);
+	return [value, onChange, onResetValue];
 }
