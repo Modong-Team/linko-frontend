86ca5c45fdceed444283bb04bacdeab2d6e3e9ab
sookyeongyeom <sookyeong.dev@gmail.com>
2023-02-21 15:02:45 +0900

    Feat: types & api for memo get & post

---
+++ @types/api/memo.d.ts
@@ -0,0 +1,32 @@
+declare namespace RequestMemo {
+	type Post = {
+		applicantId: number;
+		content: string;
+	};
+}
+
+declare namespace ResponseMemo {
+	type Get = {
+		status: number;
+		message: string;
+		code: string;
+		data: {
+			id: number;
+			applicationId: number;
+			applicantId: number;
+			creatorId: number;
+			writerId: string;
+			writerName: string;
+			content: string;
+		}[];
+	};
+
+	type Post = {
+		status: number;
+		message: string;
+		code: string;
+		data: {
+			id: number;
+		};
+	};
+}
---
+++ api/memo.ts
@@ -0,0 +1,13 @@
+import request from './core/index';
+const baseUrl = '/memo';
+
+export const getMemos = (applicantId: number) => {
+	const query = 'applicantId=' + applicantId;
+	const url = `${baseUrl}s?${query}`;
+	return request.get<ResponseMemo.Get>(url);
+};
+
+export const postMemo = (data: RequestMemo.Post) => {
+	const url = `${baseUrl}`;
+	return request.post<ResponseMemo.Post, RequestMemo.Post>(url, data);
+};
