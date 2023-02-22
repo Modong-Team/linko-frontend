73345f8c87ce18f76bb0118757adc1043d87f561
sookyeongyeom <sookyeong.dev@gmail.com>
2023-02-22 19:39:03 +0900

    Feat: match essential labels to ids

--- constants/essentials.ts
+++ constants/essentials.ts
@@ -11,7 +11,20 @@ export const Essentials = {
 
 export const EssentialCategories = {
 	default: [1, 2, 3],
+	email: [2],
+	phone: [3],
 	gender: [4],
 	birth: [5],
 	academic: [6, 7, 8],
+};
+
+export const EssentialIds = {
+	이름: 1,
+	이메일: 2,
+	전화번호: 3,
+	성별: 4,
+	생년월일: 5,
+	소속학교: 6,
+	소속과: 7,
+	학번: 8,
 };
