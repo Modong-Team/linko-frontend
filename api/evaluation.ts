28b431d35a3ef3bd7decd7d52061032c3683af97
sookyeongyeom <sookyeong.dev@gmail.com>
2023-02-23 02:36:03 +0900

    Feat: reflect changes of evaluations api

--- api/evaluation.ts
+++ api/evaluation.ts
@@ -1,33 +1,38 @@
 import request from './core/index';
-const baseUrl = '/evaluation';
+
+const baseUrl = '/applicants';
+const resource = 'evaluations';
 
 export const getEvaluations = (applicantId: number) => {
-	const query = 'applicantId=' + applicantId;
-	const url = `${baseUrl}s?${query}`;
+	const url = `${baseUrl}/${applicantId}/${resource}`;
 	return request.get<ResponseEvaluation.GetAll>(url);
 };
 
 export const getEvaluation = (applicantId: number) => {
-	const url = `${baseUrl}/${applicantId}`;
+	const url = `${baseUrl}/${applicantId}/member/${resource}`;
 	return request.get<ResponseEvaluation.Get>(url);
 };
 
-export const postEvaluation = (data: RequestEvaluation.Post) => {
-	const url = `${baseUrl}`;
+export const getEvaluationExist = (applicantId: number) => {
+	const url = `${baseUrl}/${applicantId}/exist/${resource}`;
+	return request.get<ResponseEvaluation.GetExist>(url);
+};
+
+export const postEvaluation = (applicantId: number, data: RequestEvaluation.Post) => {
+	const url = `${baseUrl}/${applicantId}/${resource}`;
 	return request.post<ResponseEvaluation.Post, RequestEvaluation.Post>(url, data);
 };
 
-export const postEvaluationCheck = (data: RequestEvaluation.PostCheck) => {
-	const url = `${baseUrl}/check`;
-	return request.post<ResponseEvaluation.PostCheck, RequestEvaluation.PostCheck>(url, data);
+export const putEvaluation = (
+	applicantId: number,
+	evaluationId: number,
+	data: RequestEvaluation.Put,
+) => {
+	const url = `${baseUrl}/${applicantId}/${resource}/${evaluationId}`;
+	return request.put<ResponseEvaluation.Put, RequestEvaluation.Put>(url, data);
 };
 
-export const putEvaluation = (data: RequestEvaluation.Put) => {
-	const url = `${baseUrl}`;
-	return request.post<ResponseEvaluation.Put, RequestEvaluation.Put>(url, data);
-};
-
-export const deleteEvaluation = (evaluationId: number) => {
-	const url = `${baseUrl}/${evaluationId}`;
+export const deleteEvaluation = (applicantId: number, evaluationId: number) => {
+	const url = `${baseUrl}/${applicantId}/${resource}/${evaluationId}`;
 	return request.delete<ResponseEvaluation.Delete>(url);
 };
