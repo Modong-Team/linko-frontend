0ff8b9e5f64bd839c47b77f1fd418da3d0599b5b
sookyeongyeom <sookyeong.dev@gmail.com>
2023-02-28 07:08:17 +0900

    Fix: clean phone number before decorate

--- utils/parsePhoneNumber.ts
+++ utils/parsePhoneNumber.ts
@@ -1,3 +1,5 @@
 export default function parsePhoneNumber(phoneNumber: string) {
-	return phoneNumber.slice(0, 3) + '-' + phoneNumber.slice(3, 7) + '-' + phoneNumber.slice(7);
+	const cleaned = phoneNumber.replaceAll('-', '');
+	const decorated = cleaned.slice(0, 3) + '-' + cleaned.slice(3, 7) + '-' + cleaned.slice(7);
+	return decorated;
 }
