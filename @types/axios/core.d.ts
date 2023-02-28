c6d4768edb967f039b8ab1f2db3f95c5796c21a6
sookyeongyeom <sookyeong.dev@gmail.com>
2023-03-01 03:43:52 +0900

    Feat: silently refresh accessToken

--- @types/axios/core.d.ts
+++ @types/axios/core.d.ts
@@ -1,8 +1,21 @@
-import { AxiosInstance, AxiosRequestConfig } from 'axios';
+import { AxiosError, AxiosInstance, AxiosRequestConfig } from 'axios';
 
 export interface GenericInstance extends AxiosInstance {
 	get<T>(url: string, config?: AxiosRequestConfig): Promise<T>;
 	post<T, U>(url: string, data: U, config?: AxiosRequestConfig): Promise<T>;
 	put<T, U>(url: string, data: U, config?: AxiosRequestConfig): Promise<T>;
 	patch<T, U>(url: string, data?: U, config?: AxiosRequestConfig): Promise<T>;
+}
+
+export interface ExtendedError extends AxiosError {
+	config: AxiosRequestConfig;
+	response: {
+		status: number;
+		data: {
+			timestamp: string;
+			code: string;
+			message: string;
+			status: number;
+		};
+	};
 }
--- api/core/index.ts
+++ api/core/index.ts
@@ -1,6 +1,10 @@
-import axios from 'axios';
-import { GenericInstance } from '../../@types/axios/core';
+import axios, { AxiosRequestConfig } from 'axios';
+import { ExtendedError, GenericInstance } from '../../@types/axios/core';
 import { store } from '../../pages/_app';
+import { postToken } from '../token';
+import { StorageKeys } from '../../constants/keys';
+import { requestSetAuthData } from '../../modules/auth/authData';
+import { ResponseCodes } from '../../constants/responseCodes';
 
 const request: GenericInstance = axios.create({
 	baseURL: process.env.NEXT_PUBLIC_BACK_END_BASE_URL,
@@ -11,12 +15,7 @@ const request: GenericInstance = axios.create({
 request.interceptors.request.use(
 	(config) => {
 		const authToken = store.getState().authData?.accessToken;
-		if (authToken) {
-			config.headers = {
-				Authorization: `Bearer ${authToken}`,
-			};
-			config.withCredentials = true;
-		}
+		if (authToken) setAuthorizedConfig(config, authToken);
 		return config;
 	},
 	(error) => {
@@ -28,7 +27,8 @@ request.interceptors.response.use(
 	(response) => {
 		return response.data;
 	},
-	(error) => {
+	(error: ExtendedError) => {
+		if (error.response.status === ResponseCodes.unauthorized) silentRefresh(error.config);
 		const code = error.response.data.code;
 		const message = error.response.data.message;
 		alert(`[${code}] ${message}`);
@@ -36,5 +36,19 @@ request.interceptors.response.use(
 		return Promise.reject(error);
 	},
 );
+
+const silentRefresh = async (config: AxiosRequestConfig) => {
+	const refreshToken = sessionStorage.getItem(StorageKeys.refreshToken);
+	if (!refreshToken) return;
+	const authData = await postToken({ refreshToken });
+	store.dispatch(requestSetAuthData(authData.data));
+	await request.request(config);
+	return;
+};
+
+const setAuthorizedConfig = (config: AxiosRequestConfig, authToken: string) => {
+	config.headers = { Authorization: `Bearer ${authToken}` };
+	config.withCredentials = true;
+};
 
 export default request;
